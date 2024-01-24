9c70211b8ec011c501f1acf85085eb153aa8f0a3
Trey0918 <122046032+Trey0918@users.noreply.github.com>
2024-01-24 14:14:16 +0000

    unfinished song melody

--- Week2dfc_music/script.js
+++ Week2dfc_music/script.js
@@ -11,4 +11,19 @@ function playNoteWithSleep(duration, note){
     
 }//close function
 
-playNoteWithSleep(77, 0.25)
+playNoteWithSleep(77, 0.25);
+playNoteWithSleep(76, 0.25);
+playNoteWithSleep(76, 0.25);
+playNoteWithSleep(77, 0.5);
+playNoteWithSleep(81, 0.25);
+playNoteWithSleep(76, 0.25);
+playNoteWithSleep(81, 0.25);
+playNoteWithSleep(79, 0.25);
+playNoteWithSleep(84, 0);
+
+function attach(){
+    const audioPlayer = document.querySelector("audio");
+    audioPlayer.src = "";
+}
+
+attach();
