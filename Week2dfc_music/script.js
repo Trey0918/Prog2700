a7cb9211ee513eb4c0abd97d9328e66a24624709
Trey0918 <122046032+Trey0918@users.noreply.github.com>
2024-01-24 13:42:01 +0000

    added styles

--- Week2dfc_music/script.js
+++ Week2dfc_music/script.js
@@ -1,5 +1,12 @@
 function playNoteWithSleep(duration, note){
     const melodyOutput = document.getElementById("melodyOutput");
-    
+   
+    melodyOutput.innerHTML += "play ${note}";
 
-}
+    setTimeout( () => (
+        melodyOutput.innerHTML += "Sleep ${duration}s"
+        
+    ) );//end timeout
+
+    
+}//close function
